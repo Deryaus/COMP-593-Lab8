8f7a286accab117cfd5f0f7cbec9672f455e8198
Geoff S <122327227+Deryaus@users.noreply.github.com>
2023-03-22 14:52:50 -0400

    Completed

--- create_relationships.py
+++ create_relationships.py
@@ -6,7 +6,10 @@ Description:
 Usage:
  python create_relationships.py
 """
-import os
+import os, sqlite3 
+from faker import Faker
+#from random import randint, choice
+import random
 
 # Determine the path of the database
 db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'social_network.db')
@@ -18,11 +21,65 @@ def main():
 def create_relationships_table():
     """Creates the relationships table in the DB"""
     # TODO: Function body
-    return
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    # SQL query that creates a table named 'relationships'.
+    create_relationships_tbl_query = """
+    CREATE TABLE IF NOT EXISTS relationships
+    (
+    id INTEGER PRIMARY KEY,
+    person1_id INTEGER NOT NULL,
+    person2_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    FOREIGN KEY (person1_id) REFERENCES people (id),
+    FOREIGN KEY (person2_id) REFERENCES people (id)
+    );
+    """
+    # Execute the SQL query to create the 'relationships' table.
+    cur.execute(create_relationships_tbl_query)
+    con.commit()
+    con.close()
+
 
 def populate_relationships_table():
     """Adds 100 random relationships to the DB"""
     # TODO: Function body
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    # SQL query that inserts a row of data in the relationships table.
+    add_relationship_query = """
+    INSERT INTO relationships
+    (
+    person1_id,
+    person2_id,
+    type,
+    start_date
+    )
+    VALUES (?, ?, ?, ?);
+    """
+    fake = Faker()
+    for new_relationship in range(100):
+        # Randomly select first person in relationship
+        person1_id = random.randint(1, 200)
+        # Randomly select second person in relationship
+        # Loop ensures person will not be in a relationship with themself
+        person2_id = random.randint(1, 200)
+        while person2_id == person1_id:
+            person2_id = random.randint(1, 200)
+        # Randomly select a relationship type
+   
+        rel_type = random.choice(('friend', 'spouse', 'partner', 'relative'))
+        # Randomly select a relationship start date between now and 50 years ago
+        start_date = fake.date_between(start_date='-50y', end_date='today')
+        # for new_relationship in range(100):
+        # Create tuple of data for the new relationship
+        new_relationship = (person1_id, person2_id, rel_type, start_date)
+        # Add the new relationship to the DB
+        cur.execute(add_relationship_query, new_relationship)
+        con.commit()
+    con.close()
+
     return 
 
 if __name__ == '__main__':
--- marriage_report.py
+++ marriage_report.py
@@ -7,7 +7,7 @@ Usage:
  python marriage_report.py
 """
 import os
-import sqlite3
+import sqlite3, pandas as pd
 from create_relationships import db_path
 
 def main():
@@ -24,8 +24,22 @@ def get_married_couples():
     Returns:
         list: (name1, name2, start_date) of married couples 
     """
-    # TODO: Function body
-    return
+    #con = sqlite3.connect('social_network.db')
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    # SQL query to get all relationships
+    all_relationships_query = """
+        SELECT person1.name, person2.name, start_date FROM relationships
+        JOIN people person1 ON person1_id = person1.id
+        JOIN people person2 ON person2_id = person2.id
+        WHERE type = "spouse";
+        
+    """
+    # Execute the query and get all results
+    cur.execute(all_relationships_query)
+    all_relationships = cur.fetchall()
+    con.close()
+    return all_relationships
 
 def save_married_couples_csv(married_couples, csv_path):
     """Saves list of married couples to a CSV file, including both people's 
@@ -35,8 +49,11 @@ def save_married_couples_csv(married_couples, csv_path):
         married_couples (list): (name1, name2, start_date) of married couples
         csv_path (str): Path of CSV file
     """
+    married_ppl_df = pd.DataFrame(married_couples)
+    header_row = ('Person 1', 'Person 2', 'Anniversary')
+    married_ppl_df.to_csv(csv_path, header=header_row, index=False)
     # TODO: Function body
-    con = sqlite3.connect(db_path)
+    
     return
 
 if __name__ == '__main__':
